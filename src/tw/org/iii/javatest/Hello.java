4ac0d78c11cdc4d13b9d88924d2a12f33e5bf9bc
chiache <chiache7410@gmail.com>
2016-10-31 12:00:27 +0800

    2016-10-31

---
+++ src/tw/org/iii/javatest/Hello.java
@@ -0,0 +1,10 @@
+package tw.org.iii.javatest;
+
+/**
+ * Created by chiache on 2016/10/31.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Word");
+    }
+}
