2529a67e072c1f5d71b72af2a4eab3e683272e52
chiache <chiache7410@gmail.com>
2016-11-01 16:32:52 +0800

    2016-11-01_test17_bike

---
+++ src/tw/org/iii/javatest/bike.java
@@ -0,0 +1,11 @@
+package tw.org.iii.javatest;
+
+public class bike {
+    double speed;
+    void upspeed(){
+
+    }
+    void downspeed(){
+
+    }
+}
---
+++ src/tw/org/iii/javatest/test17.java
@@ -0,0 +1,11 @@
+package tw.org.iii.javatest;
+
+/**
+ * Created by chiache on 2016/11/1.
+ */
+public class test17 {
+    public static void main(String[] args){
+        bike b1 = new bike();
+        System.out.println(b1.speed);
+    }
+}
