f324dd113a4b0c80250b84889ad5f01a60c369b3
chiache <chiache7410@gmail.com>
2016-11-01 16:09:30 +0800

    2016-11-01_test16_poker_攤牌

--- src/tw/org/iii/javatest/test16_poker.java
+++ src/tw/org/iii/javatest/test16_poker.java
@@ -2,6 +2,7 @@ package tw.org.iii.javatest;
 
 public class test16_poker {
     public static void main(String[] args){
+        //洗牌
         int[] poker = new int[52];
         for(int i=0;i<poker.length;i++) {
             int temp = (int)(Math.random()*poker.length);
@@ -17,9 +18,24 @@ public class test16_poker {
                 i--;
             }else {
                 poker[i] = temp;
-                System.out.println(poker[i]);
+                System.out.print(poker[i] + "  ");
             }
-
+        }
+        System.out.println();
+        System.out.println("------------------------------");
+        //發牌
+        int[][] players = new int[4][13];
+        for(int i=0;i<poker.length;i++){
+            players[i%4][i/4] = poker[i];
+        }
+        //攤牌
+        for(int[] player:players){
+            String[] suits = {"黑桃","紅心","方塊","梅花"};
+            String[] values = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+            for(int card:player){
+                System.out.print(suits[card/13] + values[card%13] + "\t");
+            }
+            System.out.println();
         }
     }
 }
